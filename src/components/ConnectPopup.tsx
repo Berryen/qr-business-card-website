69c700a175a3df68c8976ee616e5a216785c2f4d
Berryen <fooaishen@yahoo.com>
2023-12-13 18:31:08 +0800

    feat/add connect to whatsapp api

--- src/components/ConnectPopup.tsx
+++ src/components/ConnectPopup.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import clsx from "clsx";
-
 interface ConnectPopupProps {
   onClose: () => void;
+  profile: ProfileInfo;
+}
+interface ProfileInfo {
+  id: number;
+  attributes: {
+    countryCodeMobile: string;
+    countryCodeOffice: string;
+    mobileNumber: string;
+    officeNumber: string;
+    extensionNumber: string;
+  };
 }
 
-const ConnectPopup: React.FC<ConnectPopupProps> = ({ onClose }) => {
+const ConnectPopup: React.FC<ConnectPopupProps> = ({ onClose, profile }) => {
   const buttonClass = clsx(
     "w-full",
     "inline-flex",
@@ -27,19 +37,39 @@ const ConnectPopup: React.FC<ConnectPopupProps> = ({ onClose }) => {
   };
 
   const handleConnect = () => {
-    // Handle the connect action based on the selectedOption
-    // For simplicity, I'm just logging the selected option here
-    console.log("Connecting via:", selectedOption);
+    if (selectedOption === "whatsapp") {
+      // Generate WhatsApp link
+      const countryCode = getCountryCode(profile.attributes.countryCodeMobile);
+      const whatsappLink = `https://api.whatsapp.com/send?phone=${countryCode}${profile.attributes.mobileNumber}`;
+
+      // Redirect to WhatsApp link
+      window.location.href = whatsappLink;
+    } else {
+      // Handle other connect actions based on the selectedOption
+      console.log("Connecting via:", selectedOption);
+    }
 
     // Close the popup
     onClose();
+  };
+
+  const getCountryCode = (countryName: string): string => {
+    switch (countryName) {
+      case "Malaysia (60)":
+        return "60";
+      case "Singapore (65)":
+        return "65";
+      case "Vietnam (84)":
+        return "84";
+      default:
+        return countryName;
+    }
   };
 
   return (
     <div className="popup-overlay">
       <div className="popup-content w-60 sm:w-80">
         <h2 className="text-xl sm:text-2xl mb-5">Connect with</h2>
-
         <div className="flex flex-col divide-y divide-gray-300">
           <div className="flex items-center">
             <input
@@ -89,7 +119,6 @@ const ConnectPopup: React.FC<ConnectPopupProps> = ({ onClose }) => {
         </div>
 
         <div className="w-full flex gap-5 mt-5">
-          {/* Close button */}
           <button
             className={`border border-color-sky text-color-sky ex1 ${buttonClass}`}
             onClick={onClose}
